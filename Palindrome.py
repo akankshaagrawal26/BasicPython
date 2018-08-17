0e8df4106468b310b8f89f18d06acb1dba73229e
Akanksha Agrawal <akanksha.agrawal@infobeans.com>
2018-08-17 18:41:47 +0530

    palindrome program

---
+++ Palindrome.py
@@ -0,0 +1,22 @@
+
+str = input('Enter the string to be checked :') # Inputs string from the user
+
+def palindrome(str):
+    return str[::-1]
+
+check = palindrome(str)
+print("Reversed string is :",check)
+if str == check:
+    print("String entered is a palindrome string")
+else:
+    print("String entered is not a palindrome string")
+
+
+
+
+
+
+
+
+
+
