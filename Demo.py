4be4ebb19aa7c8e604985fe7f7f5c762a7b13e54
Akanksha Agrawal <akanksha.agrawal@infobeans.com>
2018-08-17 18:44:00 +0530

    function added

--- Demo.py
+++ Demo.py
@@ -53,3 +53,18 @@ print (tinydict)          # Prints complete dictionary
 print (tinydict.keys())   # Prints all the keys
 print (tinydict.values()) # Prints all the values
 
+
+def print_max(x, y):
+    '''Prints the maximum of two numbers.
+    The two values must be integers.'''
+    # convert to integers, if possible
+    x = int(x)
+    y = int(y)
+    if x > y:
+        print(x, 'is maximum')
+    else:
+        print(y, 'is maximum')
+
+print_max(3, 5)
+#print(print_max.__doc__)
+help(print_max)
