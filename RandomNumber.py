a5906e8fd34983392c5ee4fd3285efc282a6a9de
Akanksha Agrawal <akanksha.agrawal@infobeans.com>
2018-08-24 16:36:12 +0530

    random removed

--- RandomNumber.py
+++ RandomNumber.py
@@ -1,7 +1,7 @@
 import random
 from random import randrange
 
-num = random.randrange(1,101,2)    #Generating random numbers
+num = randrange(1,101,2)    #Generating random numbers
 guess = int(input("Please guess a number between 1-100 : "))
 print("Random number is :",num)
 if guess < num:
